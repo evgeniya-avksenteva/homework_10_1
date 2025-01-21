52b1360bb78e4b3b3860665cc2b6fe940bc6443a
Evgeniya Avksenteva <evgesh-092009@yandex.ru>
2025-01-21 20:56:43 +0300

    revision

--- src/widget.py
+++ src/widget.py
@@ -11,14 +11,6 @@ def mask_account_card(card: str) -> str:
         return new_card
 
 
-print(mask_account_card("Visa Platinum 7000792289606361"))
-print(mask_account_card("Maestro 7000792289606361"))
-print(mask_account_card("Счет 73654108430135874305"))
-
-
 def get_date(date: str) -> str:
     """Функция, которая возвращает строку с датой в определенном формате"""
     return f"{date[8:10]}.{date[5:7]}.{date[0:4]}"
-
-
-print(get_date("2024-03-11T02:26:18.671407"))
