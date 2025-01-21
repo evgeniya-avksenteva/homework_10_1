13a314c150c398018a34ff927a8973be23af10b8
Evgeniya Avksenteva <evgesh-092009@yandex.ru>
2025-01-21 21:03:56 +0300

    result

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from src.widget import mask_account_card, get_date
+from src.widget import get_date, mask_account_card
+
 
 print(mask_account_card("Visa Platinum 7000792289606361"))
 print(mask_account_card("Maestro 7000792289606361"))
--- src/masks.py
+++ src/masks.py
@@ -10,5 +10,5 @@ def get_mask_account(account_number: str) -> str:
     return f"**{account_number[-4:]}"
 
 
-#print(get_mask_card_number("7000792289606361"))
-#print(get_mask_account("73654108430135874305"))
+# print(get_mask_card_number("7000792289606361"))
+# print(get_mask_account("73654108430135874305"))
