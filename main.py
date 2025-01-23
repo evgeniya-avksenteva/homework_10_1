63ec62ba44cf5f3cdcbfdd438bfdeb412aacd022
Evgeniya Avksenteva <evgesh-092009@yandex.ru>
2025-01-23 22:11:11 +0300

    first function

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from src.widget import get_date, mask_account_card
+# from src.widget import get_date, mask_account_card
 
 
-print(mask_account_card("Visa Platinum 7000792289606361"))
-print(mask_account_card("Maestro 7000792289606361"))
-print(mask_account_card("Счет 73654108430135874305"))
-print(get_date("2024-03-11T02:26:18.671407"))
+# print(mask_account_card("Visa Platinum 7000792289606361"))
+# print(mask_account_card("Maestro 7000792289606361"))
+# print(mask_account_card("Счет 73654108430135874305"))
+# print(get_date("2024-03-11T02:26:18.671407"))
+
---
+++ src/processing.py
@@ -0,0 +1,17 @@
+list_dict = [
+    {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
+    {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
+    {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
+    {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
+]
+
+def filter_by_state(list_dict: list, state: str = 'EXECUTED') -> list:
+    """ Функция принимает список словарей и возвращает новый список словарей,
+    содержащий только те словари, у которых ключ state соответствует указанному значению"""
+    new_list_dict = []
+    for item_list in list_dict:
+        if item_list["state"] == state:
+            new_list_dict.append(item_list)
+    return new_list_dict
+
+#print(filter_by_state(list_dict))
