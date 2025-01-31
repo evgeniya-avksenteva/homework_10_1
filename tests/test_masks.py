96c230c309b796b14f076bf2b697d063bf6f9f47
Evgeniya Avksenteva <evgesh-092009@yandex.ru>
2025-01-31 21:45:35 +0300

    final tests

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,5 +1,4 @@
 from src.masks import get_mask_card_number, get_mask_account
-
 import pytest
 
 def test_get_mask_card_number ():
--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,8 +1,8 @@
 import pytest
-
 from src.processing import data_list, filter_by_state, sort_by_date
 
-@pytest. fixture
+
+@pytest.fixture
 def data_list() -> list:
     return [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
@@ -17,6 +17,12 @@ def test_filter_by_state(data_list: list, state: str = "EXECUTED") -> None:
             {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
             {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"}
         ]
+    elif state == "CANCELED":
+        assert filter_by_state(data_list) == [
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"}
+        ]
+
 
 @pytest. fixture
 def data_list() -> list:
