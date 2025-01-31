66b7a28d71cb0ced9845b4a38188363c154b582a
Evgeniya Avksenteva <evgesh-092009@yandex.ru>
2025-01-31 23:26:25 +0300

    final test result homework_10_2

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,9 +1,11 @@
 import pytest
-from src.processing import data_list, filter_by_state, sort_by_date
+
+from src.processing import filter_by_state, sort_by_date
 
 
 @pytest.fixture
-def data_list() -> list:
+def data_list_1() -> list:
+    """Тестирование функции на корректную фильтрацию списка словарей"""
     return [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
@@ -11,21 +13,24 @@ def data_list() -> list:
         {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
     ]
 
-def test_filter_by_state(data_list: list, state: str = "EXECUTED") -> None:
+
+def test_filter_by_state(data_list_1: list, state: str = "EXECUTED") -> None:
+    """Тестирование функции на корректную фильтрацию списка словарей по заданному статусу state"""
     if state == "EXECUTED":
-        assert filter_by_state(data_list) == [
+        assert filter_by_state(data_list_1) == [
             {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"}
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
         ]
     elif state == "CANCELED":
-        assert filter_by_state(data_list) == [
+        assert filter_by_state(data_list_1) == [
             {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"}
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
         ]
 
 
-@pytest. fixture
-def data_list() -> list:
+@pytest.fixture
+def data_list_2() -> list:
+    """Тестирование функции на корректную фильтрацию списка словарей"""
     return [
         {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
         {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
@@ -34,11 +39,12 @@ def data_list() -> list:
     ]
 
 
-def test_sort_by_date(data_list: list, sort: bool = True) -> None:
+def test_sort_by_date(data_list_2: list, sort: bool = True) -> None:
+    """Тестирование функции на корректную сортировку списка словарей по датам в порядке убывания"""
     if sort:
-        assert sort_by_date(data_list) == [
-            {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-            {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
-            {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-            {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}
+        assert sort_by_date(data_list_2) == [
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
         ]
