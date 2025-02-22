46cdc450238141e420dd16286757c6ac6a45ee8a
Evgeniya Avksenteva <evgesh-092009@yandex.ru>
2025-02-22 18:46:16 +0300

    utils financial_transactions

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,28 @@
+import json
+import os
+from json import JSONDecodeError
+from typing import Any
+
+# from src.external_api import currency_conversion
 
 
+def financial_transactions(path: str) -> Any:
+    """ Функция принимает на вход путь до JSON-файла
+    и возвращает список словарей с данными о финансовых транзакциях """
+    try:
+        with open(path, encoding="utf-8") as financial_file:
+            try:
+                transactions = json.load(financial_file)
+            except json.JSONDecodeError:
+                print("Ошибка декорирования файла")
+                return []
+        if not isinstance(transactions, list):
+                print("Файл содержит не список")
+                return []
+        return transactions
+    except FileNotFoundError:
+        print("Файл не найден")
+        return []
 
-def()
+# print(financial_transactions('../data/operations.json'))
+
